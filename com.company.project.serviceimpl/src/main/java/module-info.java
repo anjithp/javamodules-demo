c847514a2fa19a83a72c037a844df6f368e937b9
anjithp <anjith_p@yahoo.com>
2020-03-08 12:39:30 +1100

    automatic module

--- com.company.project.serviceimpl/src/main/java/module-info.java
+++ com.company.project.serviceimpl/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module com.company.project.serviceimpl {
 	requires transitive com.company.project.service;
+
+	requires httpclient;
 	
 	provides com.company.project.service.ProductService with  com.company.project.serviceimpl.ProductServiceImpl;
 	
